36cb59d392570838213ddc7539160afca1640ecd
joesanthosh24 <joe.santhosh88@yahoo.com>
2025-08-05 16:20:06 -0400

    Added basic CRUD operations for products

--- api/routes/product-routes.ts
+++ api/routes/product-routes.ts
@@ -2,8 +2,49 @@ import { Router } from 'express';
 
 const router = Router();
 
+type Product = {
+    id: number,
+    name: string,
+    price: number,
+    imageUrl: string
+}
+
+let products: Array<Product> = [];
+let id = 0;
+
 router.get('/', (req, res) => {
     res.json({ products: [] })
 });
+
+router.post('/add', (req, res) => {
+    const product = req.body;
+
+    products.push({ id: id++, name: product.name, price: product.price, imageUrl: product.imageUrl });
+
+    res.send("Added Product");
+});
+
+router.put('/:id/edit', (req, res) => {
+    const id = Number(req.params.id);
+    const index = products.findIndex(product => product.id === Number(id))
+    const { name, price, imageUrl } = req.body;
+
+    products[index] = {
+        id,
+        name,
+        price,
+        imageUrl
+    };
+
+    res.send("Edited Product with id " + id);
+});
+
+router.delete("/:id/delete", (req, res) => {
+    const id = Number(req.params.id);
+
+    products = products.filter(product => product.id !== id);
+
+    res.send("Deleted Product with id " + id);
+})
 
 export default router;
