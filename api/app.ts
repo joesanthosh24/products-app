924fbf3d9a5d2b8fb3f2a0393cd6539d63dc5ccd
joesanthosh24 <joe.santhosh88@yahoo.com>
2025-08-06 09:40:01 -0400

    Added api call to backend to fetch products. Fixed layout of product cards

--- api/app.ts
+++ api/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 import productRoutes from './routes/product-routes.ts';
 
 const app = express();
 const router = express.Router();
 
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
 app.use('/products', productRoutes);
